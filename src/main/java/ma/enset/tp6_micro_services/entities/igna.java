0764bb15f6493ea880da5399aa0f034598bfe797
bouargalne <bouargalne.hamid@gmail.com>
2023-04-20 12:26:05 +0000

    fix

---
+++ src/main/java/ma/enset/tp6_micro_services/entities/igna.java
@@ -0,0 +1,19 @@
+package ma.enset.tp6_micro_services.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class igna {
+    @Id
+    private   String tamara;
+}
